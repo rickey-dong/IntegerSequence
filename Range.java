614d76d5521064c99a613c535eec6edd909d517d
Rickey Dong <rickeyd2004@gmail.com>
2020-12-06 14:20:58 -0500

    created constructor

--- Range.java
+++ Range.java
@@ -2,5 +2,10 @@ import java.util.NoSuchElementException;
 public class Range implements IntegerSequence
 {
   private int start, end, current;
-  
+  public Range(int start, int end)
+  {
+    this.start = start;
+    this.end = end;
+    current = start;
+  }
 }
