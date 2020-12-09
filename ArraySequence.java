3105ac7ac32711fd098b92cc69dd5cd566eb079e
Rickey Dong <rickeyd2004@gmail.com>
2020-12-08 19:32:15 -0500

    made the instance variables private

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,8 +1,8 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence
 {
-  int currentIndex;
-  int[] data;
+  private int currentIndex;
+  private int[] data;
   public ArraySequence(int[] other)
   {
     data = new int[other.length];
